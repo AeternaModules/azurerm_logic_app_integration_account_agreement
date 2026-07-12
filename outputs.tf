90bed112e696b49f6a3e28b4b5e5609eb8a8a3b1
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:36 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,41 @@
-output "logic_app_integration_account_agreements" {
-  description = "All logic_app_integration_account_agreement resources"
-  value       = azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements
-}
 output "logic_app_integration_account_agreements_agreement_type" {
-  description = "List of agreement_type values across all logic_app_integration_account_agreements"
-  value       = [for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : v.agreement_type]
+  description = "Map of agreement_type values across all logic_app_integration_account_agreements, keyed the same as var.logic_app_integration_account_agreements"
+  value       = { for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : k => v.agreement_type }
 }
 output "logic_app_integration_account_agreements_content" {
-  description = "List of content values across all logic_app_integration_account_agreements"
-  value       = [for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : v.content]
+  description = "Map of content values across all logic_app_integration_account_agreements, keyed the same as var.logic_app_integration_account_agreements"
+  value       = { for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : k => v.content }
 }
 output "logic_app_integration_account_agreements_guest_identity" {
-  description = "List of guest_identity values across all logic_app_integration_account_agreements"
-  value       = [for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : v.guest_identity]
+  description = "Map of guest_identity values across all logic_app_integration_account_agreements, keyed the same as var.logic_app_integration_account_agreements"
+  value       = { for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : k => v.guest_identity }
 }
 output "logic_app_integration_account_agreements_guest_partner_name" {
-  description = "List of guest_partner_name values across all logic_app_integration_account_agreements"
-  value       = [for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : v.guest_partner_name]
+  description = "Map of guest_partner_name values across all logic_app_integration_account_agreements, keyed the same as var.logic_app_integration_account_agreements"
+  value       = { for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : k => v.guest_partner_name }
 }
 output "logic_app_integration_account_agreements_host_identity" {
-  description = "List of host_identity values across all logic_app_integration_account_agreements"
-  value       = [for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : v.host_identity]
+  description = "Map of host_identity values across all logic_app_integration_account_agreements, keyed the same as var.logic_app_integration_account_agreements"
+  value       = { for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : k => v.host_identity }
 }
 output "logic_app_integration_account_agreements_host_partner_name" {
-  description = "List of host_partner_name values across all logic_app_integration_account_agreements"
-  value       = [for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : v.host_partner_name]
+  description = "Map of host_partner_name values across all logic_app_integration_account_agreements, keyed the same as var.logic_app_integration_account_agreements"
+  value       = { for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : k => v.host_partner_name }
 }
 output "logic_app_integration_account_agreements_integration_account_name" {
-  description = "List of integration_account_name values across all logic_app_integration_account_agreements"
-  value       = [for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : v.integration_account_name]
+  description = "Map of integration_account_name values across all logic_app_integration_account_agreements, keyed the same as var.logic_app_integration_account_agreements"
+  value       = { for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : k => v.integration_account_name }
 }
 output "logic_app_integration_account_agreements_metadata" {
-  description = "List of metadata values across all logic_app_integration_account_agreements"
-  value       = [for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : v.metadata]
+  description = "Map of metadata values across all logic_app_integration_account_agreements, keyed the same as var.logic_app_integration_account_agreements"
+  value       = { for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : k => v.metadata }
 }
 output "logic_app_integration_account_agreements_name" {
-  description = "List of name values across all logic_app_integration_account_agreements"
-  value       = [for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : v.name]
+  description = "Map of name values across all logic_app_integration_account_agreements, keyed the same as var.logic_app_integration_account_agreements"
+  value       = { for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : k => v.name }
 }
 output "logic_app_integration_account_agreements_resource_group_name" {
-  description = "List of resource_group_name values across all logic_app_integration_account_agreements"
-  value       = [for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : v.resource_group_name]
+  description = "Map of resource_group_name values across all logic_app_integration_account_agreements, keyed the same as var.logic_app_integration_account_agreements"
+  value       = { for k, v in azurerm_logic_app_integration_account_agreement.logic_app_integration_account_agreements : k => v.resource_group_name }
 }
 
